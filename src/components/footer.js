5bb6276c2ccfd58812fcb5c57de043e72de1d980
P41N3ST <daniel4581@protonmail.com>
2020-08-13 18:56:02 -0500

    #A25

--- src/components/footer.js
+++ src/components/footer.js
@@ -102,7 +102,7 @@ const Footer = () => {
       </StyledSocial>
       <StyledMetadata tabindex="-1">
         <StyledGitHubLink
-          href="https://github.com/P41N3ST/v4"
+          href="https://github.com/bchiang7/v4"
           target="_blank"
           rel="nofollow noopener noreferrer">
           <div>Designed &amp; Built by Brittany Chiang</div>
